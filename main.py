ac88da959afdadd50226b99f723b2f577171b47f
Roman <romanmutel@gmail.com>
2022-02-05 21:05:32 +0200

    made folium create a map centered on Lviv

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ User specifies the year of films and the coordinates.
 '''
 
 def read_arguments():
+    '''
+    A function to read arguments using argparse module and return them as a tuple
+    '''
     parser = argparse.ArgumentParser()
     parser.add_argument('year', type=int, help='Year of the films')
     parser.add_argument('lattitude', type=float, help='Your lattitude')
@@ -16,6 +19,12 @@ def read_arguments():
     args = parser.parse_args()
     return args.year, args.lattitude, args.longtitude, args.path
 
+
+def make_map():
+    film_map = folium.Map(location=[49.8327787, 23.9421956])
+    film_map.save('nearby_films.html')
+
 if __name__ == '__main__':
     args = read_arguments()
     print(args)
+    make_map()
