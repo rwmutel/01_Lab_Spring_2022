a47198a1e557171c60b85d2f04749fc74600d6e4
Roman <romanmutel@gmail.com>
2022-02-07 18:17:03 +0200

    added geocoding via Nominatim + UA-shoot films map

--- lite.py
+++ lite.py
@@ -1,13 +1,12 @@
 with open('locations.list') as src:
-    with open('lite.list', mode='w') as new_file:
+    with open('ukraine_locations.list', mode='w') as new_file:
         line = src.readline()
         while line.strip() != '==============':
             line = src.readline()
             print(line)
-        i = 1
-        while line.strip != '':
-            line = src.readline()
-            if i % 20 == 0:
+        line = src.readline()
+        while line.strip() != '':
+            if 'Ukraine' in line:
                 new_file.write(line)
                 print(line)
-            i += 1
+            line = src.readline()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import folium
+from geopy.geocoders import Nominatim
 
 '''
 A module for creating a map showing 10 closest
@@ -21,10 +22,41 @@ def read_arguments():
 
 
 def make_map():
+    gl = Nominatim(user_agent="mutel's 01_lab project")
     film_map = folium.Map(location=[49.8327787, 23.9421956])
+    ukr = folium.FeatureGroup(name='Films shoot in Ukraine')
+
+    with open('ukraine_locations.list') as src:
+        data = src.readlines()
+        known_locations = dict()
+        for film in data:
+            film = film.strip().split('\t')
+            coordinates = ''
+            location = film[-1]
+
+            if film[-1].startswith('('):
+                location = film[-2]
+
+            if location in known_locations:
+                coordinates = known_locations[location]
+            else:
+                coordinates = gl.geocode(location)
+                i = 1
+                while coordinates is None:
+                    coordinates = gl.geocode(','.join(location.split(', ')[i:]))
+                    i += 1
+                known_locations[location] = coordinates
+
+            print(location, film[0])
+            ukr.add_child(folium.Marker(name=film[0], 
+                location=(coordinates.latitude, coordinates.longitude), 
+                popup=folium.Popup()))
+
+    film_map.add_child(ukr)
+    film_map.add_child(folium.LayerControl())
     film_map.save('nearby_films.html')
 
 if __name__ == '__main__':
+    make_map()
     args = read_arguments()
     print(args)
-    make_map()
